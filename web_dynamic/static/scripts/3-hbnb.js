0b6a5256825bb635404f6e16249d52b807664066
David-Inkheart <okoliedave1@gmail.com>
2023-02-23 21:52:36 +0100

    refactored

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,6 +1,68 @@
-// fetches and prints how to say “Hello” depending on the language
-
 $(document).ready(function () {
+  const placesUri = 'http://localhost:5001/api/v1/places_search/';
+  const $placesSection = $('section.places');
+
+  function createMaxGuests (guests) {
+    if (guests !== 1) {
+      return `<div class="max_guest">${guests} Guests</div>`;
+    }
+
+    return `<div class="max_guest">${guests} Guest</div>`;
+  }
+
+  function createRooms (rooms) {
+    if (rooms !== 1) {
+      return `<div class="number_rooms">${rooms} Bedrooms</div>`;
+    }
+
+    return `<div class="number_rooms">${rooms} Bedroom</div>`;
+  }
+
+  function createBathrooms (bathrooms) {
+    if (bathrooms !== 1) {
+      return `<div class="number_bathrooms">${bathrooms} Bathrooms</div>`;
+    }
+
+    return `<div class="number_bathrooms">${bathrooms} Bathroom</div>`;
+  }
+
+  function createArticleTitle (title, price) {
+    return `<div class="title_box">
+              <h2>${title}</h2>
+              <div class="price_by_night">
+                $${price}
+              </div>
+            </div>`;
+  }
+
+  function createArticleInfo (guests, rooms, bathrooms) {
+    const maxGuests = createMaxGuests(guests);
+    const maxRooms = createRooms(rooms);
+    const maxBathrooms = createBathrooms(bathrooms);
+    return `<div class="information">
+              ${maxGuests}
+              ${maxRooms}
+              ${maxBathrooms}
+            </div>`;
+  }
+
+  function createArticleDescription (description) {
+    return `<div class="description">
+              ${description}
+            </div>`;
+  }
+
+  function createPlace (place) {
+    const articleTitle = createArticleTitle(place.name, place.price_by_night);
+    const articleInfo = createArticleInfo(place.max_guest, place.number_rooms, place.number_bathrooms);
+    const articleDescription = createArticleDescription(place.description);
+    return `<article>
+              ${articleTitle}
+              ${articleInfo}
+              ${articleDescription}
+            </article>`;
+  }
+
   const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
   $.get(url, function (response) {
     if (response.status === 'OK') {
@@ -20,15 +82,16 @@ $(document).ready(function () {
     $('H4#sweet').text(Object.values(amenities).join(', '));
   });
 
-  const request = 'http://0.0.0.0:5001/api/v1/places_search/';
-  $.post(request,
-    {
-      headers: 'Content-Type: application/json',
-      data: {}
-    },
-    function (data, textStatus) {
-      for (let i = 0; i < data.length; i++) {
-        $('section.places').prepend('<article></article>').text('prepend() data[i]');
+  $.ajax({
+    url: placesUri,
+    type: 'POST',
+    dataType: 'json',
+    data: '{}',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; ++i) {
+        $placesSection.append(createPlace(data[i]));
       }
-    });
+    }
+  });
 });
